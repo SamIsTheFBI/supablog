db75a85fbb389b1271087443e1fdc39000372b99
Shivam Shekhar Soy <shivamshekharsoy@gmail.com>
2024-03-20 15:59:21 +0530

    fix: removed page numbers from pagination

--- src/components/blog/blog-card.tsx
+++ src/components/blog/blog-card.tsx
@@ -1,9 +1,12 @@
+import { getAuthorById } from "@/server/actions/blogActions";
 import { SelectBlogs } from "@/server/db/schema/blog";
 import Image from "next/image";
 import Link from "next/link";
 import { LuCalendar } from "react-icons/lu";
+import { RxPerson } from "react-icons/rx";
 
-export default function BlogCard({ post }: { post: SelectBlogs }) {
+export default async function BlogCard({ post }: { post: SelectBlogs }) {
+  const author = await getAuthorById(post.authorId)
   return (
     <div className="rounded-md border space-y-3 h-full w-full hover:bg-secondary bg-card text-card-foreground group">
       <Link href={`/blog/${post.slug}`} className="space-y-2 flex flex-col p-5">
@@ -19,13 +22,19 @@ export default function BlogCard({ post }: { post: SelectBlogs }) {
             />
           </div>
         }
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold tracking-tighter">
           {post.title}
         </h1>
-        <span className="inline-flex items-start gap-x-1 text-sm text-muted-foreground">
-          <LuCalendar size={15} />
-          {post.createdAt.toLocaleString('en-US', { day: '2-digit', month: 'short', year: 'numeric' })}
-        </span>
+        <div className="inline-flex items-start gap-x-4 text-sm text-muted-foreground">
+          <span className="flex items-start gap-x-1">
+            <LuCalendar size={15} />
+            {post.createdAt.toLocaleString('en-US', { day: '2-digit', month: 'short', year: 'numeric' })}
+          </span>
+          <span className="flex items-start gap-x-1">
+            <RxPerson size={15} />
+            {author.name}
+          </span>
+        </div>
         <p>
           {post.description}
         </p>
--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -74,7 +74,7 @@ const PaginationPrevious = ({
     {...props}
   >
     <ChevronLeftIcon className="h-4 w-4" />
-    <span className="max-sm:hidden">Previous</span>
+    <span>Previous</span>
   </PaginationLink>
 )
 PaginationPrevious.displayName = "PaginationPrevious"
@@ -89,7 +89,7 @@ const PaginationNext = ({
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
-    <span className="max-sm:hidden">Next</span>
+    <span>Next</span>
     <ChevronRightIcon className="h-4 w-4" />
   </PaginationLink>
 )
