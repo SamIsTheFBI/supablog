c984d859b8da7111c69d2b16afe7afbecd5cf687
Shivam Shekhar Soy <shivamshekharsoy@gmail.com>
2024-09-09 23:30:13 +0530

    remove todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import AddTodoForm from "@/components/home/add-todo-form";
-import TodoItem from "@/components/home/todo-item";
 import Header from "@/components/layout/Header";
-import { Card } from "@/components/ui/card";
-import { getData } from "@/server/actions/todoActions";
 
 export default async function Home() {
-  const todos = await getData()
   return (
     <>
       <Header />
@@ -15,16 +10,6 @@ export default async function Home() {
             A generic template for Nextjs apps.
           </p>
           <p className="text-muted-foreground">I am trying to build a starter layout kind of thing for my Nextjs projects.</p>
-        </div>
-        <div className="max-w-96 mx-auto px-2">
-          <Card className="p-4 space-y-4">
-            <AddTodoForm />
-            <ul className="space-y-2">
-              {todos.length > 0 && todos.map((todo) => (
-                <TodoItem key={todo.id} id={todo.id} todo={todo.text} />
-              )) || <span>No items</span>}
-            </ul>
-          </Card>
         </div>
       </main>
     </>
